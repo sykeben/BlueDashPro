25904a5c95a5d403619d4537e41668f14af1cab9
Benjamin Sykes <33205078+sykeben@users.noreply.github.com>
2019-03-22 21:44:45 -0400

    Minor fix.
    - Fixed issue that would prevent all settings from loading if there was no stream url saved.

--- js/setuploader.js
+++ js/setuploader.js
@@ -3,7 +3,9 @@ function loadSetup() {
     
     if (getSetting("teamkey") != null) document.getElementsByName("input-teamkey")[0].value = getSetting("teamkey").toString();
     if (getSetting("eventkey") != null) document.getElementsByName("input-eventkey")[0].value = getSetting("eventkey").toString();
-    if (getSetting("streamurl") != "[none]" || getSetting("streamurl") != null) document.getElementsByName("input-streamurl")[0].value = getSetting("streamurl").toString();
+    if (getSetting("streamurl") != null) {
+        if (getSetting("streamurl") != "[none]") document.getElementsByName("input-streamurl")[0].value = getSetting("streamurl").toString();
+    }
     if (getSetting("showsponsers") == 1) $("#input-showsponsers").prop("checked", true);
     
     document.getElementById("value-apikey").innerHTML = getSetting("apikey");
